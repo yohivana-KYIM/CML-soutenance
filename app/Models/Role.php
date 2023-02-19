808d599f5ebdbc44dcf0bde7af9c0ab2e5e6d668
Youta Obby Sidane <dyosby237@gmail.com>
2023-02-19 19:54:44 +0100

    Fix Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Models;
-//use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
@@ -13,6 +12,6 @@ class Role extends Model
 
     public function users()
     {
-        return $this->hasMany(User::class, 'user_id');
+        return $this->hasMany(User::class, 'role_id');
     }
 }
